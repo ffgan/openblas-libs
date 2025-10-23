0658865adad4cd0aae7dff7e94e0592dd00765e7
ffgan <sudoemt@gmail.com>
2025-10-23 16:23:50 +0800

    fix command error for woa

--- tools/repair_for_win.sh
+++ tools/repair_for_win.sh
@@ -1,14 +1,14 @@
 set -xe
 pip install delvewheel
 
-if ( ${OS-NAME} -eq "windows-latest" ) {
+if [[ ${OS-NAME} -eq "windows-latest" ]]; then
     # we don't need to rename the wheel for windows-latest as it have correct platform tag
     # for f in dist/*.whl; 
     #     do mv $f "${f/%any.whl/$WHEEL_PLAT.whl}";
     # done
     delvewheel repair -w $1 $2
     exit 0
-}
+fi
 
 # repair for windows arm64
 ./tools/repair_wheel_for_win_arm64.bat $1 $2
