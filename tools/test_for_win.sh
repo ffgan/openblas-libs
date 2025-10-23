1a96d077624aa5da955f54bcb9f8382798fa9d04
ffgan <sudoemt@gmail.com>
2025-10-23 11:43:42 +0800

    fix test error for win

--- tools/test_for_win.sh
+++ tools/test_for_win.sh
@@ -1,17 +1,9 @@
-if [[ ${OS-NAME} == "windows-latest" ]];then
-    if [[ "${INTERFACE64}" == "1" ]]; then
-        # python -m pip install --no-index --find-links dist scipy_openblas64
-        python -m scipy_openblas64
-        python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config())"
-    else
-        # python -m pip install --no-index --find-links dist scipy_openblas32
-        python -m scipy_openblas32
-        python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())"
-    fi
-    exit 0
+if [[ "${INTERFACE64}" == "1" ]]; then
+    # python -m pip install --no-index --find-links dist scipy_openblas64
+    python -m scipy_openblas64
+    python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config())"
+else
+    # python -m pip install --no-index --find-links dist scipy_openblas32
+    python -m scipy_openblas32
+    python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())"
 fi
-
-# test for windows arm64
-# python -m pip install --no-index --find-links dist scipy_openblas${env:INTERFACE_BITS}
-python -m scipy_openblas${env:INTERFACE_BITS}
-python -c "import scipy_openblas${env:INTERFACE_BITS}; print(scipy_openblas${env:INTERFACE_BITS}.get_pkg_config())"
